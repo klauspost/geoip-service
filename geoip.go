6f9eb4c85e99a155d68a923491dae5a0c06129b4
klauspost <klauspost@gmail.com>
2014-11-25 10:56:37 +0100

    Add result cache. Defaults to keeping results 60 seconds. Cached results are about twice as fast as uncached.

--- geoip.go
+++ geoip.go
@@ -4,16 +4,19 @@ import (
 	"encoding/json"
 	"flag"
 	"github.com/oschwald/geoip2-golang"
+	"github.com/pmylund/go-cache"
 	"log"
 	"net"
 	"net/http"
 	"runtime"
 	"strings"
+	"time"
 )
 
 type Response struct {
-	Data  interface{} `json:",omitempty"`
-	Error string      `json:",omitempty"`
+	Data   interface{} `json:",omitempty"`
+	Error  string      `json:",omitempty"`
+	cached bool
 }
 
 func main() {
@@ -22,9 +25,14 @@ func main() {
 	var listen = flag.String("listen", ":5000", "Listen address and port, for instance 127.0.0.1:5000")
 	var threads = flag.Int("threads", runtime.NumCPU(), "Number of threads to use. Defaults to number of detected cores")
 	var pretty = flag.Bool("pretty", false, "Should output be formatted with newlines and intentation")
+	var cacheSecs = flag.Int("cache", 60, "How many seconds should requests be cached. Set to 0 to disable")
 	flag.Parse()
 
 	runtime.GOMAXPROCS(*threads)
+	var memCache *cache.Cache
+	if *cacheSecs > 0 {
+		memCache = cache.New(time.Duration(*cacheSecs)*time.Second, 1*time.Second)
+	}
 	lookupCity := true
 	if *lookup == "country" {
 		lookupCity = false
@@ -43,6 +51,7 @@ func main() {
 	prettyL := *pretty
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		var ipText string
 		// We don't need the body
 		req.Body.Close()
 
@@ -59,10 +68,13 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
+			if memCache != nil && !res.cached {
+				memCache.Set(ipText, j, 0)
+			}
 			w.Write(j)
 		}()
 
-		ipText := req.URL.Query().Get("ip")
+		ipText = req.URL.Query().Get("ip")
 		if ipText == "" {
 			ipText = strings.Trim(req.URL.Path, "/")
 		}
@@ -71,6 +83,14 @@ func main() {
 			res.Error = "unable to decode ip"
 			w.WriteHeader(http.StatusBadRequest)
 			return
+		}
+		if memCache != nil {
+			v, found := memCache.Get(ipText)
+			if found {
+				res.cached = true
+				res.Data = json.RawMessage(v.([]byte))
+				return
+			}
 		}
 		if lookupCity {
 			result, err := db.City(ip)
